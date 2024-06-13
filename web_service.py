29a7a20305b430c4385489d5e8b5e6589b460df2
alphia <alphia@163.com>
2024-06-13 19:06:49 +0800

    add openai compatible api for caption

--- web_service.py
+++ web_service.py
@@ -21,6 +21,33 @@ def load_image(image_file):
     return image
 
 
+@app.route('v1/chat/completions', methods=['POST'])
+def captioning_open_ai_compatible():
+    data = request.get_json()
+    sampling = data['sampling'] > 0 if 'sampling' in data else True
+    temperature = data['temperature'] if 'temperature' in data else 0.1
+    messages = data['messages'] if 'messages' in data else []
+    first_img_url, new_messages = extract_url_and_messages(messages)
+    caption = model.chat(first_img_url, json.dumps(new_messages, ensure_ascii=True), sampling=sampling,
+                         temperature=temperature)
+    return jsonify({'caption': caption})
+
+
+def extract_url_and_messages(messages):
+    new_messages = []
+    image_urls = []
+    for message in messages:
+        if isinstance(message['content'], list) and message['role'] == 'user':
+            for item in message:
+                if item['type'] == 'text':
+                    new_messages.append({"role": message['role'], "content": item['text']})
+                if item['type'] == 'image_url':
+                    image_urls.append(item['image_url']['url'])
+        if isinstance(message['content'], str):
+            new_messages.append(message)
+    return (image_urls[0]), new_messages
+
+
 @app.route('/', methods=['POST'])
 def captioning():
     data = request.get_json()
